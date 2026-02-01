8ddaead4da398c73c417533b850daf725daa5a22
EBO'O Clément <eboojuniorclement@gmail.com>
2026-02-01 05:51:28 +0100

    Migrate from GLFW to SDL for window management

--- main.cpp
+++ main.cpp
@@ -1,27 +1,52 @@
-#include <GLFW/glfw3.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_opengl.h>
 #include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_sdl3.h"
 #include "backends/imgui_impl_opengl3.h"
+#include <stack>
 
-void init_imgui(GLFWwindow* window) {
+void init_imgui(SDL_Window* window, SDL_GLContext gl_context) {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
     ImGui::StyleColorsDark();
-
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init("#version 130");
 }
 
 void render_imgui() {
     ImGui::NewFrame();
 
-    ImGui::Begin("Visualisation des Structures de Données");
+    ImGui::Begin("Data Structure Visualizer");
 
-    // Ajoute des widgets pour afficher les données, comme des tables, des arbres, etc.
-    ImGui::Text("Voici un exemple de visualisation d'une pile");
+    // Visualisation de la pile
+    static std::stack<int> stack;
+    if (ImGui::Button("Push to Stack")) {
+        stack.push(rand() % 100); // Ajouter un nombre aléatoire
+    }
+
+    ImGui::Text("Stack: ");
+    std::stack<int> temp_stack = stack; // Copie de la pile pour l'affichage
+    while (!temp_stack.empty()) {
+        ImGui::Text("%d", temp_stack.top());
+        temp_stack.pop();
+    }
+
+    // Visualisation du tableau
+    static int arr[5] = {1, 2, 3, 4, 5};
+    ImGui::Text("Array: ");
+    ImGui::Columns(2);
+    ImGui::Text("Index");
+    ImGui::NextColumn();
+    ImGui::Text("Value");
+    ImGui::NextColumn();
+    for (int i = 0; i < 5; ++i) {
+        ImGui::Text("%d", i);
+        ImGui::NextColumn();
+        ImGui::Text("%d", arr[i]);
+        ImGui::NextColumn();
+    }
+    ImGui::Columns(1);
 
     ImGui::End();
 
@@ -29,45 +54,58 @@ void render_imgui() {
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-int main() {
-    if (!glfwInit()) {
-        std::cerr << "Erreur lors de l'initialisation de GLFW!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Visualizer", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        std::cerr << "Erreur lors de la création de la fenêtre GLFW!" << std::endl;
+    SDL_Window* window = SDL_CreateWindow("DataStructureVisualizer",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          1280, 720, SDL_WINDOW_OPENGL);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
         return -1;
     }
 
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Erreur lors du chargement d'OpenGL!" << std::endl;
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    if (!gl_context) {
+        std::cerr << "SDL_GL_CreateContext Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
         return -1;
     }
 
-    init_imgui(window);
+    // Initialisation de ImGui
+    init_imgui(window, gl_context);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            ImGui_ImplSDL3_ProcessEvent(&event);
+        }
 
+        // Rendu OpenGL
         glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         render_imgui();
 
-        glfwSwapBuffers(window);
+        SDL_GL_SwapWindow(window);
     }
 
+    // Nettoyage
     ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
